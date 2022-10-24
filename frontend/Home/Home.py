50356c1c9c87a8dea968df4743879c1b7991d1cb
Rafael-Silva-Oliveira <dbiorafael@gmail.com>
2022-10-24 11:51:38 +0100

    changes

--- frontend/Home/Home.py
+++ frontend/Home/Home.py
@@ -2,19 +2,11 @@
 ######## version = 1.0
 ######## status = WIP
 ###########################################################################################################
-import sys
 import streamlit as st
 import numpy as np
 import pandas as pd
-import pickle
-import time
-from matplotlib import pyplot as plt
-from  matplotlib.ticker import FuncFormatter
 import seaborn as sns
-import pyarrow.parquet as pq
 import datetime
-from st_aggrid import AgGrid, GridOptionsBuilder
-from st_aggrid.shared import GridUpdateMode
 import matplotlib.pyplot as plt
 st.set_page_config(layout="wide")
 from load_css import local_css
@@ -22,9 +14,7 @@ local_css("style.css")
 import io
 import plotly.express as px
 import plotly.graph_objects as go
-import base64
 from PIL import Image
-import pendulum
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from dateutil.relativedelta import relativedelta
@@ -35,10 +25,8 @@ from io import BytesIO
 buffer = io.BytesIO()
 import io
 import xlsxwriter
-import wx
 import streamlit.components.v1 as components
 import requests
-from termcolor import colored
 import hydralit_components as hc
 hc.hydralit_experimental(True)
 
